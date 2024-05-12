b130ee76991a5b2625662a6ff8080d6bc0fc857f
Kaya-Sem <kayasemvc@gmail.com>
2024-05-12 21:46:03 +0200

    read urls from text etc

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,19 +1,34 @@
+import argparse
 import feedparser
 import subprocess
 
-RSS_FEED_URL = "https://www.youtube.com/feeds/videos.xml?channel_id=UCaiVt4r6YLPzJVgr7pOmD6w"
-
 def download_video(url):
-    subprocess.Popen(["yt-dlp", url])
+    subprocess.run(["yt-dlp", url])
 
-def main():
-    feed = feedparser.parse(RSS_FEED_URL)
+def parseURLS(path) :
+    with open(path) as file:
+        return file.readlines()
+
+def main(filepath):
+
+    urls = parseURLS(filepath)
+
+    for index, item in enumerate(urls):
+        print(f"parsing url {index + 1}: {item}\n")
+        feed = feedparser.parse(item)
     
-    for entry in feed.entries:
-        url = entry.link
+        for entry in feed.entries:
+           url = entry.link
         
-        if "youtube.com" in url or "youtu.be" in url:
-            download_video(url)
+           if "youtube.com" in url or "youtu.be" in url:
+               download_video(url)
+               break # allow only the last video to be downloaded
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Download videos from YouTube RSS feeds.")
+    parser.add_argument("filepath", help="Path to the file containing URLs.")
+
+    args = parser.parse_args()
+
+    main(args.filepath)
+
