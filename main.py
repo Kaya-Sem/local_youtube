9318b40e33aa8daa48d7b2b545a1e21f4c8ab13b
Kaya-Sem <kayasemvc@gmail.com>
2024-05-13 11:47:26 +0200

    extracted argument checking from main

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
+
 import argparse
 import feedparser
 import subprocess
 
-# Author: Kaya-Sem
-#
-#
-# simple script that can read a youtube creator's rss and downloads their most recent videos
-
 DEFAULT_AMOUNT_VIDS = 2
 DEFAULT_URLS_PATH = "template_urls.txt"
-
-# TODO create template shell command that people can implement and override themselves
-# TODO  download location
-# TODO allow comments in urls file
-
 
 def download_video(url):
     subprocess.run(["yt-dlp", url])
@@ -21,17 +12,9 @@ def download_video(url):
 def parseURLfile(path):
     with open(path) as file:
         lines = [line.strip() for line in file if line.strip() and not line.strip().startswith("//")]
-
     return lines
 
-def main(filepath, num_videos=None):
-
-    if filepath is None:
-        filepath = DEFAULT_URLS_PATH
-
-    if num_videos is None:
-        num_videos = DEFAULT_AMOUNT_VIDS
-
+def main(filepath, num_videos):
     urls = parseURLfile(filepath)
     num_downloaded = 0
 
@@ -46,7 +29,7 @@ def main(filepath, num_videos=None):
                 download_video(url)
                 num_downloaded += 1
                 if num_downloaded >= num_videos:
-                    return  # Stop when the desired number of videos have been downloaded
+                    return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download videos from YouTube RSS feeds.")
@@ -55,5 +38,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args.urls, args.num_videos)
+    urls_path = args.urls if args.urls is not None else DEFAULT_URLS_PATH
+    num_videos = args.num_videos if args.num_videos is not None else DEFAULT_AMOUNT_VIDS
+
+    main(urls_path, num_videos)
 
